388196afa3ae4956000d43377358fd4d89167933
Abderrahmane Maftah <78481502+mafghal@users.noreply.github.com>
2023-02-24 08:43:17 +0000

    t

--- ajout.php
+++ ajout.php
@@ -49,23 +49,34 @@
                             <form method='POST' action=''>
                                 <div class="mb-3">
                                     <label class="form-label">Titre d'anonce</label>
-                                    <input type="text" class="form-control" name="Titre">
+                                    <input type="text" class="form-control" name="T_Annonce">
                                     <span class="text-danger"></span>
                                 </div>
                                 <div class="mb-3">
                                     <label class="form-label">Prix</label>
-                                    <input type="number" class="form-control" name="prix">
+                                    <input type="number" class="form-control" name="P_Annonce">
                                     <span class="text-danger"></span>
                                 </div>
                                 <div class="mb-3">
                                     <label class="form-label">Date de la publication : </label>
-                                    <input type="date" class="form-control" name="prix">
+                                    <input type="date" class="form-control" name="Date_Pub">
                                     <span class="text-danger"></span>
                                 </div>
                                 <div class="mb-3">
                                     <label class="form-label">Adresse : </label>
-                                    <textarea class="form-control" name="prix" cols="30" rows="2"></textarea>
+                                    <textarea class="form-control" name="A_Annonce" cols="30" rows="2"></textarea>
                                     <span class="text-danger"></span>
+                                </div>
+                                <div class="mb-3">
+                                    <label class="form-label">Ville </label>
+                                    <select class="form-select" aria-label="ville" name="ville">
+                                        <option></option>
+                                        <option value="Tangier">Tangier</option>
+                                        <option value="Tetouan">Tetouan</option>
+                                        <option value="Casablanca">Casablanca</option>
+                                        <option value="Rabat">Rabat</option>
+                                        <option value="Marrakech">Marrakech</option>
+                                    </select>
                                 </div>
                                 <div class="mb-3">
                                     <label class="form-label">Titre d'anonce</label>
@@ -74,7 +85,7 @@
                                 </div>
                                 <div class="mb-3">
                                     <label class="form-label">Categorie d'annonce : </label>
-                                    <select class="form-select" aria-label="type" name="categorie">
+                                    <select class="form-select" aria-label="type" name="C_Annonce">
                                         <option></option>
                                         <option value="Location">Location</option>
                                         <option value="Vente">Vente</option>
@@ -82,7 +93,7 @@
                                 </div>
                                 <div class="mb-3">
                                     <label class="form-label">Type d'annonce : </label>
-                                    <select class="form-select" aria-label="type" name="type">
+                                    <select class="form-select" aria-label="type" name="Type_Annonce">
                                         <option></option>
                                         <option value="appartement">appartement</option>
                                         <option value="maison">maison</option>
@@ -92,8 +103,14 @@
                                     </select>
                                 </div>
                                 <div class="mb-3">
+                                <input class="form-control"type="hidden"  value="30000"  name="CH_Image" multiple/>
+                                <input name="CH_Image" type="file" multiple/>
+                            </div>
+                                
+
+                                <div class="mb-3">
                                     <label class="form-label">Superficie : </label>
-                                    <input type="number" class="form-control" name="prix">
+                                    <input type="number" class="form-control" name="superficie">
                                     <span class="text-danger"></span>
                                 </div>
                                                     
@@ -180,25 +197,6 @@
                     
         ?> -->
 
-
-        <script>
-                    function() {
-                'use strict'
-                const forms = document.querySelectorAll('.requires-validation')
-                Array.from(forms)
-            .forEach(function (form) {
-                form.addEventListener('submit', function (event) {
-                if (!form.checkValidity()) {
-                    event.preventDefault()
-                    event.stopPropagation()
-                }
-
-                form.classList.add('was-validated')
-                }, false)
-            })
-            })()
-
-        </script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
